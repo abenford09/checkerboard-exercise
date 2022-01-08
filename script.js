aaa1edf61f6a0b0739c2207623b78e2821c89eef
Alfred <85899437+abenford09@users.noreply.github.com>
2022-01-08 10:43:51 -0600

    1/08/2022

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your JS goes here
+function checkerboard() {
+    // let body = document.getElementsByTagName('body')[0]
+    // let tiles = document.createElement('tiles');
+    console.log('this is a div called tiles');
+    for (var i = 0; i < 64; i++) {
+        if (i % 2 === 0) {
+            
+            let even = document.createElement('even')
+            even.style.width = `11.1%`;
+            even.style.height = `11.1%`;
+            even.style.paddingBottom = `11.1%`;
+            even.style.backgroundColor = "red"
+            even.style.float = "left";
+            document.body.appendChild(even);
+        } else {
+            let odd = document.createElement("odd")
+            even.style.width = `11.1%`;
+            tiles.style.height = `11.1%`;
+            tiles.style.paddingBottom = `11.1%`;
+            tiles.style.backgroundColor = "black"
+            tiles.style.float = "left";
+            document.body.appendChild(odd);
+        }
+    }
+}
+
+checkerboard()
