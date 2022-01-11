42b5777768c21cae9e3f1d33ce805c14217052dc
Alfred <85899437+abenford09@users.noreply.github.com>
2022-01-11 16:47:18 -0600

    1/11/2022

--- gradient.js
+++ gradient.js
@@ -1,21 +1,33 @@
-function checkerboard() {
-    // Loop and set param for board
-    for (var i = 0; i < 64; i++) {
+var red= 40;
+var blue= 120;
+var green;
+function gradient(i){
+      if (i % 2 === 0) {
+          red += 3;
+          green = 50;
+      } else {
+          green = 150;
+      }
+      return "rgb(" + red + "," + green + "," + blue + ")";
+  }
+
+  function checkerboard() {
+    for (let i = 0; i < 63; i++) {
         if (i % 2 === 0) {
-            var even = document.createElement('even')
+            let even = document.createElement('even')
             document.body.appendChild(even);
             even.style.width = `11.1%`;
             even.style.height = `11.1%`;
             even.style.paddingBottom = `11.1%`;
-            even.style.backgroundColor = "red"
+            even.style.backgroundColor = gradient(i)
             even.style.float = "left";
         } else {
-            var odd = document.createElement('odd')
+            let odd = document.createElement('odd')
             document.body.appendChild(odd);
             odd.style.width = `11.1%`;
             odd.style.height = `11.1%`;
             odd.style.paddingBottom = `11.1%`;
-            odd.style.backgroundColor = "black"
+            odd.style.backgroundColor = gradient(i)
             odd.style.float = "left";
             console.log('test')
         }
--- random.js
+++ random.js
@@ -1,21 +1,30 @@
+// build function that gets random colors
+function randomColor() {
+    // hex color random
+    let color = '#';
+    for (let i = 0; i < 3; i++) {
+        color += ('0' + (Math.random()*256|0).toString(16)).substr(-2);
+      }
+      return color;
+}
+
 function checkerboard() {
-    // Loop and set param for board
-    for (var i = 0; i < 64; i++) {
+    for (let i = 0; i < 64; i++) {
         if (i % 2 === 0) {
-            var even = document.createElement('even')
+            let even = document.createElement('even')
             document.body.appendChild(even);
             even.style.width = `11.1%`;
             even.style.height = `11.1%`;
             even.style.paddingBottom = `11.1%`;
-            even.style.backgroundColor = "red"
+            even.style.backgroundColor = randomColor()
             even.style.float = "left";
         } else {
-            var odd = document.createElement('odd')
+            let odd = document.createElement('odd')
             document.body.appendChild(odd);
             odd.style.width = `11.1%`;
             odd.style.height = `11.1%`;
             odd.style.paddingBottom = `11.1%`;
-            odd.style.backgroundColor = "black"
+            odd.style.backgroundColor = randomColor()
             odd.style.float = "left";
             console.log('test')
         }
